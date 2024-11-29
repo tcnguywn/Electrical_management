296139a2d53734c4eca2269d07a54dd42a67b95a
tcnguyen <nguyentruong260804@gmail.com>
2024-11-30 00:32:34 +0700

    permitall for ELECTRICIAN

--- electricity/src/main/java/com/dev/electricity/configuration/SecurityConfig.java
+++ electricity/src/main/java/com/dev/electricity/configuration/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(request ->
                 request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
+                        .requestMatchers("/**").hasRole("ELECTRICIAN")
                         .anyRequest().authenticated());
 
         httpSecurity.oauth2ResourceServer(oauth2 ->
